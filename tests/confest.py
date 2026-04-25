a6535694a39d4b1532f4199cd6059cb2853baefe
strengen <cherkass800322@gmail.com>
2026-04-25 19:58:58 +0200

    #100

--- tests/confest.py
+++ tests/confest.py
@@ -1,8 +1,11 @@
 import pytest
 from core.database import db, create_tables
+from peewee import SqliteDatabase
 
+db = SqliteDatabase(None)
 @pytest.fixture(autouse=True)
 def setup_db():
+    db.init(':memory:')
     db.connect()
     create_tables()
     yield
