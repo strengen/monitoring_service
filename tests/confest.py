2c85f451ff2c1d23eab1cbdbfb3dce1c1ca8d487
strengen <cherkass800322@gmail.com>
2026-04-25 21:30:21 +0200

    added back confest.py

---
+++ tests/confest.py
@@ -0,0 +1,12 @@
+import pytest
+from core.database import db, Book, PriceHistory
+
+@pytest.fixture(autouse=True)
+def setup_db():
+    db.connect(reuse_if_open=True)
+    db.create_tables([Book, PriceHistory])
+
+    yield
+
+    db.drop_tables([Book, PriceHistory])
+    db.close()
