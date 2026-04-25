c8d8ab0c2ccc9d8ffa77599ec91ba0d988be5d58
strengen <cherkass800322@gmail.com>
2026-04-25 20:38:14 +0200

    #1 changed confest and db initialization

--- tests/confest.py
+++ tests/confest.py
@@ -1,12 +1,14 @@
 import pytest
-from core.database import db_proxy
 from peewee import SqliteDatabase
 
 @pytest.fixture(autouse=True)
 def setup_db():
     db = SqliteDatabase(':memory:')
     db.connect()
-    db.create_tables(['Book', 'PriceHistory'])
+
+    from core.database import Book, PriceHistory
+    db.create_tables([Book, PriceHistory])
+    
     yield
     db.drop_tables(db.get_tables())
     db.close()
